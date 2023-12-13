ba15a33065f434d448f67694ecb9914f39aa64c4
카니보어 코리아 <152141122+CarnivoreKR@users.noreply.github.com>
2023-12-13 18:54:18 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Carnivore",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png",
     role: "Carnivore Korea",
     bio: "카니보어 한국 커뮤니티",
     email: "contact@carnivore.kr",
